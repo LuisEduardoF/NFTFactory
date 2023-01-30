369f651207c8126f7ab9a2f1ea5bf19f3e6ec9f2
Luis Câmara <luisefcamara@gmail.com>
2023-01-30 15:04:27 +0000

    Beta changes

---
+++ script.js
@@ -0,0 +1,172 @@
+// 1. Declaração de variável global para armazenar a instância web3
+let NFTFactoryContract;
+
+// 2. Configuração do endereço do contrato e ABI
+const NFTFactory_Address = "0x492b83acCA4C0c5773937407412d177c8FE8059b";
+const NFTFactory_ABI = [
+	{
+		"inputs": [],
+		"name": "checkIfOwner",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_symbol",
+				"type": "string"
+			}
+		],
+		"name": "createNFT",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getNFTs",
+		"outputs": [
+			{
+				"internalType": "contract NFT[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "nftContract",
+		"outputs": [
+			{
+				"internalType": "contract NFT",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "nftName",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "nftSymbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+/* 3. Prompt user to sign in to MetaMask */
+const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+provider.send("eth_requestAccounts", []).then(() => {
+    provider.listAccounts().then((accounts) => {
+        const signer = provider.getSigner(accounts[0]);
+
+        /* 3.1 Create instance of pet smart contract */
+        NFTFactoryContract = new ethers.Contract(
+            NFTFactory_Address,
+            NFTFactory_ABI,
+            signer
+        );
+    });
+});
+const nft_create = document.getElementById("create_div");
+const nft_access = document.getElementById("access_div");
+const nft_create_button = document.getElementById("create");
+const nft_access_button = document.getElementById("access_but");
+
+var isOwner = false;
+var nftsymbol = "Your NIFT";
+var nftContract = "";
+const checkIfOwner = async() => {
+    try {
+        const owns = await NFTFactoryContract.checkIfOwner();
+		const symbol = await NFTFactoryContract.nftSymbol();
+		const nft = await NFTFactoryContract.nftContract();
+        isOwner = owns;
+		nftsymbol = symbol;
+		nftContract = nft;
+		console.log(nftContract)
+    } catch (e) {}
+}
+var onetime = true;
+function checkOwner() {
+    checkIfOwner();
+    if (isOwner && nftsymbol != "Your NIFT" && onetime && window.location.href.search("index") != -1) {
+		nft_access.style.display = "block";
+		nft_create.style.display = "none";
+		nft_access_button.innerHTML += nftsymbol;
+		onetime = false;
+    }
+}
+setInterval(checkOwner, 1000)
+
+// 4. Creating variables for reusable dom elements
+
+/* 5. Function to set pet details */
+const createNIFT = () => {
+    // update button value
+    nft_create_button.value = "Creating NIFT...";
+
+    /* 5.1 Get inputs from pet form */
+    const nft_name = document.querySelector("#Name");
+    const nft_symbol = document.querySelector("#Symbol");
+
+    // 5.2 Getting values from the inputs
+    NFTname = nft_name.value;
+    NFTsymbol = nft_symbol.value;
+
+    /* 5.3 Set pet details in smart contract */
+    NFTFactoryContract.createNFT(NFTname, NFTsymbol)
+        .then(() => {
+        })
+        .catch((err) => {
+            nft_create_button.value = "Create a NIFT";
+            alert("Error setting pet details" + err.message);
+        });
+};
+const accessNIFT = () => {
+	window.location.href = window.location.href.replace("index", "collection.html?contract="+nftContract)
+}
+
+nft_access_button.addEventListener("click", accessNIFT)
+
+/* Function to set pet details on click of button */
+nft_create_button.addEventListener("click", createNIFT);
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,502 @@
+// 1. Declaração de variável global para armazenar a instância web3
+let NFTContract;
+
+// 2. Configuração do endereço do contrato e ABI
+const NFT_Address = new URLSearchParams(window.location.search).get("contract").replace(".html", "");
+const NFT_ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_symbol",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "approved",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "ApprovalForAll",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "tokenURI",
+				"type": "string"
+			}
+		],
+		"name": "NewQuote",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "collection",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "getApproved",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getCollection",
+		"outputs": [
+			{
+				"internalType": "string[]",
+				"name": "",
+				"type": "string[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			}
+		],
+		"name": "isApprovedForAll",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "tokenURI",
+				"type": "string"
+			}
+		],
+		"name": "mint",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "ownerOf",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "safeTransferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "data",
+				"type": "bytes"
+			}
+		],
+		"name": "safeTransferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "setApprovalForAll",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes4",
+				"name": "interfaceId",
+				"type": "bytes4"
+			}
+		],
+		"name": "supportsInterface",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "tokenURI",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
+
+var nftSymbol = ""
+var nftName = ""
+var owner = ""
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+provider.send("eth_requestAccounts", []).then(() => {
+    provider.listAccounts().then(async (accounts) => {
+        const signer = provider.getSigner(accounts[0]);
+
+        /* 3.1 Create instance of pet smart contract */
+        NFTContract = new ethers.Contract(
+            NFT_Address,
+            NFT_ABI,
+            signer
+        );
+        nftSymbol = await NFTContract.symbol()
+        owner = await NFTContract.owner();
+        document.getElementById("titleName").innerHTML += " " + nftSymbol
+    
+        NFTContract.on('NewQuote', function (event) {
+            updateQuotes(event)
+        });
+        
+        var quotes = await NFTContract.getCollection()
+        for(var i = 0; i < quotes.length; i++){
+            updateQuotes(quotes[i])
+        }
+    });
+});
+
+var postQuote_button = document.getElementById("postQuote")
+
+const createQuote = () => {
+    // update button value
+    postQuote_button.value = "Creating Quote...";
+
+    /* 5.1 Get inputs from pet form */
+    const quote = document.getElementById("quote").value;
+
+    /* 5.3 Set pet details in smart contract */
+    NFTContract.mint(quote)
+        .then(() => {
+        })
+        .catch((err) => {
+            postQuote_button.value = "Create a NIFT";
+        });
+};
+
+postQuote_button.addEventListener("click", createQuote)
+
+last_update_counter = 0
+function updateQuotes(tokenURI) {
+        var col = document.createElement('col');
+        col.innerHTML = `
+        <figure class="bg-light p-4"
+        style="border-left: .35rem solid #fcdb5e; border-top: 1px solid #eee; border-right: 1px solid #eee; border-bottom: 1px solid #eee;">
+            <blockquote class="blockquote pb-2">
+                <p id="quote_${last_update_counter}">
+                    ${"\"" + tokenURI + "\""}
+                </p>
+            </blockquote>
+            <figcaption class="blockquote-footer mb-0" id="owner">
+                ${owner}
+            </figcaption>
+        </figure>`;
+        document.getElementById('row').appendChild(col);
+        last_update_counter +=1
+}
+
+
+
+
